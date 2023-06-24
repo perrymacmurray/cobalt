84bbc48cc6d85d94c783f0543521aa774fa274f5
Perry MacMurray <perrymacmurray@gmail.com>
2023-06-23 22:50:20 -0500

    cobalt setup changes

--- cobalt/bin/cobalt.lua
+++ cobalt/bin/cobalt.lua
@@ -1,1 +1,27 @@
-print("This does nothing yet");
+local fs = require("filesystem");
+local component = require("component");
+
+local internet = component.internet;
+
+print("Welcome to the Cobalt setup!");
+
+if (not fs.exists("/etc/cobalt/uid")) then
+    print("This computer currently does not have a UID.")
+    print("Please enter the desired UID, or press enter to generate a new one")
+    local uid = io.read();
+    if (uid == "" or uid == nil) then
+        print("Generating UID...");
+        uid = require("uuid").next();
+        print("UUID:", uid);
+    end
+    fs.makeDirectory("/etc/cobalt");
+    io.open("/etc/cobalt/uid", "w");
+    io.write(uid);
+    io.close();
+end
+
+if (internet != nil) then
+    --todo
+elseif
+    print("WARNING: This computer cannot connect to the Internet, severely limiting Cobalt's functionality.")
+end
