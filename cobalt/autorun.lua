6af118b88791af6532ce52ee30727986f2b8420d
Perry MacMurray <perrymacmurray@gmail.com>
2023-06-23 22:39:22 -0500

    Minor changes

--- cobalt/autorun.lua
+++ cobalt/autorun.lua
@@ -1,11 +1,12 @@
 local component = require("component");
 local fs = require("filesystem");
-
+local term = require("term");
 local computer = component.computer;
 
-if (!fs.exists("/etc/cobalt/uid")) then
-    print("Cobalt is not configured properly, and will not run.")
-    print("Run the cobalt command for setup")
+if (not fs.exists("/etc/cobalt/uid")) then
+    term.clear();
+    print("Cobalt is not configured properly, and will not run.");
+    print("Run the cobalt command for setup");
     computer.beep(1000, 0.5);
     computer.beep(2000, 0.5);
     computer.beep(1000, 0.5);
