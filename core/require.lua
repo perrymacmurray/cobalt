76467be8d345dcd2cda41db00d13c101e4cd5a39
Perry MacMurray <perrymacmurray@gmail.com>
2024-04-29 20:02:56 -0500

    Fix require bugs

--- core/require.lua
+++ core/require.lua
@@ -3,14 +3,16 @@ local require = function(file)
     local f = fs.open(file)
     if f == nil then return nil end
 
-    repeat
+    while true do
         local data = f:read(math.maxinteger)
+        if data == nil then break end
+
         buffer = buffer .. data
-    until not data
+    end
 
     f:close()
 
-    return load(buffer, "=" .. file, "bt", _G)()
+    return load(buffer, "=" .. file, "bt", _G)
 end
 
 return require
